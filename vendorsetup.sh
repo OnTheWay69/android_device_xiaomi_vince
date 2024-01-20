931343acb8fb425be4db3afb0b9f88e1c4638dab
Dutaiclik <nuraly042000@gmail.com>
2024-01-20 02:31:31 +0000

    Initial vendorsetup
    Signed-off-by: Dutaiclik <nuraly042000@gmail.com>

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone --depth=1 https://github.com/OnTheWay69/android_vendor_xiaomi_vince vendor/xiaomi/vince
+git clone --depth=1 https://github.com/Dutaiclik/kernel_xiaomi_vince -b nksu kernel/xiaomi/vince
+git clone --deoth=1 https://gitlab.com/cherishos-tiramisu/vince-clang-17.git prebuilts/clang/host/linux-x86/clang-aosp
